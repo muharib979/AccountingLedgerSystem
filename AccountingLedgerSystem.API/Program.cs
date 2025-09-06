2f81fb8079ea3a01c88a1ecf09ec1ada272e4a7c
Muhammad Amran Bhuiyan <35559798+muharib979@users.noreply.github.com>
2025-09-06 11:50:29 +0600

    Update Program.cs

--- AccountingLedgerSystem.API/Program.cs
+++ AccountingLedgerSystem.API/Program.cs
@@ -31,6 +31,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.UseHttpsRedirection();
 
